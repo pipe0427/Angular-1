ec922192e9a0b247aa5f8803a19bbf5ac102a951
pipe0427 <pipe.04271@gmail.com>
2023-08-14 22:21:15 -0500

    proyecto terminado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'easy-form';
+  
+  title = 'Formulario Eam';
+  name = 'Nombre';
+  surname = 'Apellido';
+  select = 'Genero';
+  email = 'Email';
+  bond = 'Cedula';
+  masculino = 'Masculino';
+  femenino = 'Femenino';
+
+  name_nuevo = '';
+  surname_nuevo = '';
+  select_nuevo = '';
+  email_nuevo = '';
+  bond_nuevo = '';
+
+  guardarDatos(){
+      this.name_nuevo = this.name;
+      this.surname_nuevo = this.surname;
+      this.select_nuevo = this.select;
+      this.email_nuevo = this.email;
+      this.bond_nuevo = this.bond;
+  }
 }
