34f3dded236f27a49bcee295daa2a2a785e7310e
pipe0427 <pipe.04271@gmail.com>
2023-08-16 17:48:06 -0500

    mejora de pagina

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,14 +7,21 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   
-  title = 'Formulario Eam';
-  name = 'Nombre';
-  surname = 'Apellido';
-  select = 'Genero';
-  email = 'Email';
-  bond = 'Cedula';
+  title = 'Escuela de administracion y mercadotecnia';
+  name = '';
+  surname = '';
+  select = '';
+  email = '';
+  bond = '';
   masculino = 'Masculino';
   femenino = 'Femenino';
+
+  nombre = 'Nombre';
+  apellido = 'Apellido';
+  genero = 'Genero';
+  correo = 'Email';
+  cedula = 'Cedula';
+
 
   name_nuevo = '';
   surname_nuevo = '';
@@ -22,11 +29,18 @@ export class AppComponent {
   email_nuevo = '';
   bond_nuevo = '';
 
+  url = 'https://www.eam.edu.co/wp-content/uploads/2021/10/logoo.png';
+
   guardarDatos(){
       this.name_nuevo = this.name;
+      this.name = '';
       this.surname_nuevo = this.surname;
+      this.surname = '';
       this.select_nuevo = this.select;
+      this.select = '';
       this.email_nuevo = this.email;
+      this.email = '';
       this.bond_nuevo = this.bond;
+      this.bond = '';
   }
 }
